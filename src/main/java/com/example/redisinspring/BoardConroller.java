b9f283780957a295bf4c5fe257fadaa1ba2b88a2
JiksGit <2wlrlwkd@gmail.com>
2025-01-29 15:00:55 +0900

    redis 강의 실습

--- src/main/java/com/example/redisinspring/BoardConroller.java
+++ src/main/java/com/example/redisinspring/BoardConroller.java
@@ -1,2 +1,24 @@
-package com.example.redisinspring;public class BoardConroller {
+package com.example.redisinspring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("boards")
+public class BoardConroller {
+    private BoardService boardService;
+
+    public BoardConroller(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping()
+    public List<Board> getBoards(
+            @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size){
+        return boardService.getBoards(page, size);
+    }
 }
